aa2fb6e13440276297006e1e68654e808e82325e
2eunnnn <eunchaei868@gmail.com>
2024-06-25 15:53:29 +0900

    change code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const fetchData = (FilE_NAME) => {
   const res = axios.get(
     // 자신의 깃허브 주소 입력
     // https://raw.githubusercontent.com/'자신 깃허브 이름'/simulation/main/simulation/src/data/${FilE_NAME}.json
-    `https://raw.githubusercontent.com/2eunnnn/Assignment/main/src/data/${FilE_NAME}.json`
+    `https://raw.githubusercontent.com/2eunnnn/simulation/main/simulation/src/data/${FilE_NAME}.json`
+    
   );
   const data = res.then((r) => r.data);
   return data;
